c7a2d4ab7f0239adf8a0d62ed50719aa789aebc9
Caua Brito <cauabsp@id.uff.br>
2026-04-01 14:47:58 -0300

    Tarefa 2: Adicionando histograma e grafico de dispersao

--- analise_imoveis.R
+++ analise_imoveis.R
@@ -1,3 +1,16 @@
 library(readr)
 imoveis <- read_delim("imoveis.csv", delim = ";")
 summary(imoveis)
+# Histograma do Preço do Imóvel
+hist(imoveis$preco, 
+     main="Histograma de Preços", 
+     xlab="Preço", 
+     col="lightblue")
+
+# Gráfico de Dispersão: Imposto (y) vs Metragem (x)
+plot(x = imoveis$metragem, 
+     y = imoveis$imposto_anual, 
+     main="Dispersão: Imposto vs Metragem", 
+     xlab="Metragem (x)", 
+     ylab="Imposto (y)", 
+     pch=19, col="darkblue")
